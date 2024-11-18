83f9c7313735eebde0a73d89da79218671e7b6b3
paxelotl <141200190+paxelotl@users.noreply.github.com>
2024-11-18 00:30:26 -0800

    First successful HTTP request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    basic().await?;
+
+    Ok(())
+}
+
+async fn basic() -> Result<(), Box<dyn std::error::Error>> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+    println!("body = {:?}", body);
+
+    Ok(())
 }

51bd2d47d2fa251175e8bb88949b76d5dac0d4eb
paxelotl <141200190+paxelotl@users.noreply.github.com>
2025-01-24 01:22:26 -0800

    Add libcosmic prospectively

--- shell.nix
+++ shell.nix
@@ -15,6 +15,16 @@ pkgs.mkShell rec {
     pkg-config
     openssl
     libiconv
+
+    # libcosmic deps
+    cargo
+    cmake
+    just
+    expat
+    fontconfig
+    freetype
+    libxkbcommon
+    pkgconf
   ];
   RUSTC_VERSION = overrides.toolchain.channel;
   # https://github.com/rust-lang/rust-bindgen#environment-variables
